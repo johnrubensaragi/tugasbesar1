b5629514e2d74b5e67a5b9c7712e05bb5bd5c10d
johnrubensaragi <71202283+johnrubensaragi@users.noreply.github.com>
2022-10-22 19:20:10 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ name_file = open("name.txt", "r+")
 id_other_file = open("id_other.txt", "r+")
 name_other_file = open("name_other.txt", "r+")
 bank_list_file = open("bank.txt", "r")
+trial_file = open("trial.txt", "r+")
 
 id_list = []
 for i in id_file.readlines():
@@ -37,6 +38,10 @@ name_other_list = []
 for i in name_other_file.readlines():
     name_other_list.append(i.strip())
 
+trial_list = []
+for i in trial_file.readlines():
+    trial_list.append(i.strip())
+
 nama_bank = {"009" : "BNI",  "002" : "BRI", "008" : "MANDIRI", "011" : "DANAMON", "013" : "PERMATA", "014" : "BCA",
  		    "016" : "MAYBANK", "019" : "BANK PANIN", "022" : "CIMB NIAGA", "028" : "OCB NISP", "031" : "CITIBANK",
             "036" : "CCBINDONESIA", "037" : "ARTHA GRAHA", "046" : "BANK DBS", "050" : "STANCHART", "054" : "BANK CAPITAL",
@@ -57,8 +62,10 @@ kamus = {
     "PIN yang Anda masukkan salah."         : "Wrong PIN.",
     "Anda sudah memasukkan PIN yang salah sebanyak tiga kali. Akun Anda terblokir."
                                             : "You entered wrong PIN(s) three times. Your account is now blocked.",
-    "Menu penarikan cepat (1-5): "          : "Quick withdrawal menu (1-5): ",
-    "Menu lain (1-5): "                     : "Other menu (1-5): ",
+    "Menu penarikan cepat"          : "Quick withdrawal menu: ",
+    "5. Menu penarikan cepat"          : "5. Quick withdrawal menu: ",
+    "Menu lain"                     : "Other menu: ",
+    "Pilih menu (1-5): "                    : "Option (1-5): ",
     "Masukkan nominal uang (rupiah): "      : "Enter the nominal (rupiah): ",
     "1. Penarikan tunai"                    : "1. Withdrawal",
     "2. Transfer"                           : "2. Transfer",
@@ -69,7 +76,8 @@ kamus = {
     "Apakah Anda ingin mencetak resi? (y/n): ": "Do you want a receipt? (y/n): ", 
     "Pilihan tidak tersedia."               : "Option is not available.",
     "Transfer berhasil."                    : "Transfer successful.",
-    "Transaksi tidak dapat diproses."       : "Transantion cannot be processed." }
+    "Transaksi tidak dapat diproses."       : "Transantion cannot be processed.",
+    "Apakah Anda ingin mengganti PIN? (y/n): ": "Do you want to change your PIN? (y/n)"}
 
 def tr(a):                      #Untuk menterjemahkan suatu string
     if bahasa == "1":
@@ -80,17 +88,41 @@ def tr(a):                      #Untuk menterjemahkan suatu string
         else:
             return "TRANSLATION ERROR"
 
+def id_validation():
+    os.system('cls')
+    id = input(tr("Masukkan nomor rekening: "))
+    while not(id in id_list):
+        os.system('cls')
+        print(tr("ID Anda tidak dikenali."))
+        id = input(tr("Masukkan nomor rekening: "))
+    indeks = id_list.index(id)
+
+def pin_validation():
+    os.system('cls')
+    pin = pwinput.pwinput(prompt = tr("Masukkan PIN: "))
+    pin_sha256 = sha256(pin.encode('utf-8')).hexdigest()
+    percobaan = 1
+    while percobaan < 3 and pin_sha256 != pin_list[indeks]:
+        os.system('cls')
+        print(tr("PIN yang Anda masukkan salah."))
+        pin = pwinput.pwinput(prompt = tr("Masukkan PIN: "))
+        pin_sha256 = sha256(pin.encode('utf-8')).hexdigest()
+        percobaan += 1
+    if percobaan == 3:
+        print(tr("Anda sudah memasukkan PIN yang salah sebanyak tiga kali. Akun Anda terblokir."))
+        exit()
+
 def halaman1():
-    print("1. Rp250.000,00", "2. Rp500.000,00", "3. Rp1.000.000,00", "4. Rp1.250.000,00", tr("5. Menu lain"), tr("6. Batal"), sep="\n")
+    print(tr("Menu penarikan cepat"),"1. Rp250.000,00", "2. Rp500.000,00", "3. Rp1.000.000,00", "4. Rp1.250.000,00", tr("5. Menu lain"), tr("6. Batal"), sep="\n")
 
 def halaman2():
-    print(tr("1. Penarikan tunai"), tr("2. Transfer"), tr("3. Ganti PIN"), tr("4. Informasi saldo"), tr("5. Menu lain"), tr("6. Batal"), sep="\n")
+    print(tr("Menu lain"), tr("1. Penarikan tunai"), tr("2. Transfer"), tr("3. Ganti PIN"), tr("4. Informasi saldo"), tr("5. Menu penarikan cepat"), tr("6. Batal"), sep="\n")
 
 def main():
     while True:
         os.system('cls')
         halaman1()
-        pilih_menu = input(tr("Menu penarikan cepat (1-5): "))
+        pilih_menu = input(tr("Pilih menu (1-5): "))
         if pilih_menu == "1":
             penarikan_tunai(25000000)
         elif pilih_menu == "2":
@@ -103,11 +135,16 @@ def main():
             while True:
                 os.system('cls')
                 halaman2()
-                pilih_menu = input(tr("Menu lain (1-5): "))
+                pilih_menu = input(tr("Pilih menu (1-5): "))
                 if pilih_menu == "1":
                     os.system('cls')
-                    nominal = int(input(tr("Masukkan nominal uang (rupiah): ")))*100
-                    penarikan_tunai(nominal*100)
+                    try:
+                        nominal = int(input(tr("Masukkan nominal uang (rupiah): ")))*100
+                    except:
+                        print(tr("Nominal tidak sesuai"))
+                        input()
+                        main()
+                    penarikan_tunai(nominal)
                 elif pilih_menu == "2":
                     transfer()
                 elif pilih_menu == "3":
@@ -125,6 +162,10 @@ def main():
         else:
             print(tr("Pilihan tidak tersedia."))
     
+def restart():
+    pin_validation()
+    main()
+
 def ubah_format(awal):
     if len(awal) == 1:
         return "0,0"+awal
@@ -148,7 +189,7 @@ def ubah_format(awal):
     return akhir
 
 def penarikan_tunai(nominal):
-    if (nominal % 5000000) == 0:
+    if (nominal % 5000000) == 0 and nominal > 0:
         if nominal <= balances_list[indeks]:
             balances_list[indeks] -= nominal
             print(tr("Penarikan berhasil dilakukan."))
@@ -169,11 +210,12 @@ def penarikan_tunai(nominal):
                 print("-"*42)
                 input()
                 os.system('cls')
-            ganti_pin_tanda_tanya = input(tr("Apakah Anda ingin mengganti password? (y/n): "))
+            ganti_pin_tanda_tanya = input(tr("Apakah Anda ingin mengganti PIN? (y/n): "))
             if ganti_pin_tanda_tanya == "y":
                 ganti_pin()
+                main()
             else:
-                exit()
+                restart()
         else:
             print(tr("Maaf, saldo Anda tidak mencukupi."))
             input()
@@ -237,8 +279,9 @@ def ubah(jenis_bank, id_tujuan, indeks_tujuan, nominal):
         nominal = int(input("Masukkan nominal transfer: "))*100
         
 def transfer():
+    # CEK DULU NOMINALNYA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
     os.system('cls')
-    print("1. BNI", "2. Bank lain", sep="\n")
+    print("1. BNI", tr("2. Bank lain"), sep="\n")
     jenis_bank = input(tr("Pilih tujuan transfer yang Anda inginkan: "))
     while jenis_bank != "1" and jenis_bank != "2":
         os.system('cls')
@@ -283,11 +326,11 @@ def transfer():
     else:
         while konfirmasi_transfer(id_tujuan[3:], name_other_list[indeks_tujuan], nominal) != "1":
             ubah(jenis_bank, id_tujuan, indeks_tujuan, nominal)
-    if nominal >= 100:
+    if nominal >= 5000000:
         if nominal <= balances_list[indeks]:
-            balances_list[indeks] -= nominal*100
+            balances_list[indeks] -= nominal
             if jenis_bank == "1":
-                balances_list[indeks_tujuan] += nominal*100
+                balances_list[indeks_tujuan] += nominal
                 balances_file.seek(0)
                 for i in balances_list:
                     balances_file.writelines(str(i) + "\n")
@@ -324,7 +367,7 @@ def transfer():
 
 def pin_valid(pin):
     if pin.isnumeric():
-        if int(pin) >= 0 and int(pin) < 1000000:
+        if len(pin) == 6:
             return True
     return False
 
@@ -363,19 +406,11 @@ def ganti_pin():
             print(tr("Kedua PIN baru Anda tidak sesuai."))
     else:
         print(tr("PIN lama Anda tidak sesuai."))
+    exit()
         
 def informasi_saldo():
     print("SALDO REKENING:" + f" Rp {ubah_format(str(balances_list[indeks]))}")
     input()
-
-# Pemilihan Bahasa
-print("1. Indonesia", "2. English", sep="\n")
-bahasa = input("Pilih bahasa / Choose a language: ")
-while bahasa != "1" and bahasa != "2":
-    os.system('cls')
-    print("Pilihan tidak tersedia./Option is not available.")
-    print("1. Indonesia", "2. English", sep="\n")
-    bahasa = input("Pilih bahasa / Choose a language: ")
 
 """
 bahasa = ""
@@ -389,27 +424,68 @@ while bahasa != "1" and bahasa != "2":
 
 # Validasi ID
 os.system('cls')
-id = input(tr("Masukkan nomor rekening: "))
-while not(id in id_list):
+id = input("Masukkan nomor rekening Anda / Enter your account number: ")
+while not(id in id_list) or not(id.isdecimal()):
     os.system('cls')
-    print(tr("ID Anda tidak dikenali."))
-    id = input(tr("Masukkan nomor rekening: "))
+    if "*"+id in id_list:
+        print("Nomor rekening Anda terblokir. / Your account is blocked.")
+    else:
+        print("Nomor rekening Anda tidak dikenali. / Your account is not recognized.")
+    id = input("Masukkan nomor rekening Anda / Enter your account number: ")
 indeks = id_list.index(id)
+
+# Pemilihan Bahasa
+os.system('cls')
+print("1. Indonesia", "2. English", sep="\n")
+bahasa = input("Pilih bahasa / Choose a language: ")
+while bahasa != "1" and bahasa != "2":
+    os.system('cls')
+    print("Pilihan tidak tersedia./Option is not available.")
+    print("1. Indonesia", "2. English", sep="\n")
+    bahasa = input("Pilih bahasa / Choose a language: ")
 
 # Validasi PIN
 os.system('cls')
 pin = pwinput.pwinput(prompt = tr("Masukkan PIN: "))
+while not pin_valid(pin):
+    os.system('cls')
+    print(tr("PIN harus terdiri dari 6 digit."))
+    pin = pwinput.pwinput(prompt = tr("Masukkan PIN: "))
 pin_sha256 = sha256(pin.encode('utf-8')).hexdigest()
-percobaan = 1
+percobaan = int(trial_list[indeks])+1
+trial_list[indeks] = str(percobaan)
+trial_file.seek(0)
+for i in trial_list:
+    trial_file.writelines(i + "\n")
+trial_file.truncate()
 while percobaan < 3 and pin_sha256 != pin_list[indeks]:
     os.system('cls')
     print(tr("PIN yang Anda masukkan salah."))
+    print(tr("Jika Anda salah memasukkan PIN sebanyak 3 (tiga) kali, akun Anda akan terblokir"))
     pin = pwinput.pwinput(prompt = tr("Masukkan PIN: "))
+    while not pin_valid(pin):
+        os.system('cls')
+        print(tr("PIN harus terdiri dari 6 digit."))
+        pin = pwinput.pwinput(prompt = tr("Masukkan PIN: "))
     pin_sha256 = sha256(pin.encode('utf-8')).hexdigest()
     percobaan += 1
-if percobaan == 3:
-    print(tr("Anda sudah memasukkan PIN yang salah sebanyak tiga kali. Akun Anda terblokir."))
+    trial_list[indeks] = str(percobaan)
+    trial_file.seek(0)
+    for i in trial_list:
+        trial_file.writelines(i + "\n")
+    trial_file.truncate()
+if percobaan == 3 and pin_sha256 != pin_list[indeks]:
+    id_list[indeks] = "*" + id_list[indeks]
+    id_file.seek(0)
+    for i in id_list:
+        id_file.writelines(i + "\n")
+    id_file.truncate()
+    print(tr("Anda sudah memasukkan PIN yang salah sebanyak 3 (tiga) kali. Akun Anda terblokir."))
     exit()
-
+trial_list[indeks] = "0"
+trial_file.seek(0)
+for i in trial_list:
+    trial_file.writelines(i + "\n")
+trial_file.truncate()
 # Penampilan dan Pemilihan Menu
 main()
